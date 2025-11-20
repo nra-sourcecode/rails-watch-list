c8b61f2d3451864ea9ede50ed8d52b36950317d2
Nora Drentje <webbestelling@protonmail.com>
2025-11-20 11:36:32 +0100

    added validations

---
+++ app/models/movie.rb
@@ -0,0 +1,9 @@
+class Movie < ApplicationRecord
+  has_many :bookmarks
+  has_many :lists, through: :bookmarks
+
+  validates :title, uniqueness: true
+  validates :overview, uniqueness: true
+  validates :title, presence: true
+  validates :overview, presence: true
+end
