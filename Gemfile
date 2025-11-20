d5b1efa7a2970c1aae25491ec3d8ccaaa66edf09
Nora Drentje <webbestelling@protonmail.com>
2025-11-20 12:01:11 +0100

    seeded the db

--- Gemfile
+++ Gemfile
@@ -30,6 +30,8 @@ gem "autoprefixer-rails"
 gem "font-awesome-sass", "~> 6.1"
 gem "simple_form"
 gem "sassc-rails"
+
+gem "httparty"
 # Use Redis adapter to run Action Cable in production
 # gem "redis", ">= 4.0.1"
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,11 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+require "httparty"
+
+movies_api = HTTParty.get("https://tmdb.lewagon.com/movie/top_rated")
+results = movies_api["results"]
+
+results.each do |result|
+ puts Movie.create(title: result["original_title"], overview: result["overview"], poster_url: 'https://image.tmdb.org/t/p/original/#{result["poster_path"]}', rating: result["vote_average"])
+end
